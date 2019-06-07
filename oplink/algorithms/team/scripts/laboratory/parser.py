d8fa4d77452a904e2abe82771000e5295a2484ca
marreA <alemarrerodiaz@gmail.com>
2019-06-07 12:23:01 +0100

    Parser working with different args

--- oplink/algorithms/team/scripts/laboratory/parser.py
+++ oplink/algorithms/team/scripts/laboratory/parser.py
@@ -1,5 +1,6 @@
 
 import json
+import itertools
 
 # Constantes de un experimento
 JOBS = "jobs"
@@ -13,6 +14,7 @@ OUTPUT_FILE = "OutputFilePattern"
 PRINTER = "Printer"
 REPS = "Repetitions"
 PROB_ARGS = "ProblemArgs"
+
 ALG_ARGS = "AlgorithmArgs"
 MUTATION = "Mutation"
 CROSSOVER = "Crossover"
@@ -21,7 +23,7 @@ DECOMPOSITION = "Decomposition"
 
 PROBLEM_SEP = "!"
 LOCAL_SEARCH_SEP = "$"
-
+MUTATION_SEP = "-"
 METCO_PATH_ = "/home/marrero/software-metco/oplink/algorithms/team/src/skeleton/main/"
 METCO_EXEC_ = "/home/marrero/software-metco/oplink/algorithms/team/src/skeleton/main/metcoSeq"
 
@@ -41,13 +43,38 @@ class Parser:
     def __create_jobs(self, jobs):
         for job in jobs:
             command = None
-            for rep in range(job[REPS]):
-                command = f".{METCO_EXEC_} {job[OUTPUT_DIR]} {METCO_PATH_} "
-                command += f"{job[PRINTER]} {job[OUTPUT_FILE]}_{rep} "
-                command += f"{job[ALG]} {job[PROB]} {job[STOP_CRITERIA]} "
-                command += f"{job[STOP_LIMIT]} {job[STEPS]} 0 "
-                command += f"{LOCAL_SEARCH_SEP} {job[LOCAL_SEARCH]} "
-                self.experiment_list.append(command)
+            alg_args = []
+            for arg in job[ALG_ARGS]:
+                alg_args.append(job[ALG_ARGS][arg])
+            prob_args = []
+            for arg in job[PROB_ARGS]:
+                prob_args.append(job[PROB_ARGS][arg])
+
+            alg_params_combs = list(itertools.product(*alg_args))
+            prob_params_combs = list(itertools.product(*prob_args))
+
+            for alg_comb in alg_params_combs:
+                for prob_comb in prob_params_combs:
+                    for rep in range(job[REPS]):
+                        command = f".{METCO_EXEC_} {job[OUTPUT_DIR]} {METCO_PATH_} "
+                        command += f"{job[PRINTER]} {job[OUTPUT_FILE]}_{rep} "
+                        command += f"{job[ALG]} {job[PROB]} {job[STOP_CRITERIA]} "
+                        command += f"{job[STOP_LIMIT]} {job[STEPS]} 0 "
+                        # Parametros del algoritmo vienen aqui
+                        command += " ".join(str(x) for x in alg_comb) + " "
+                        command += PROBLEM_SEP + " " + \
+                            " ".join(str(x) for x in prob_comb) + " "
+                        # Opciones que pueden estar o no
+                        if MUTATION in job and CROSSOVER in job:
+                            command += f"{MUTATION_SEP} {job[MUTATION]} "
+                            command += f"{job[CROSSOVER]} "
+                        elif MUTATION in job and CROSSOVER not in job:
+                            command += f"{MUTATION_SEP} {job[MUTATION]} "
+                        elif MUTATION not in job and CROSSOVER in job:
+                            command += f"{MUTATION_SEP} {job[CROSSOVER]} "
+
+                        command += f"{LOCAL_SEARCH_SEP} {job[LOCAL_SEARCH]} "
+                        self.experiment_list.append(command)
         return self.experiment_list
 
     def __parse_instance(self):
