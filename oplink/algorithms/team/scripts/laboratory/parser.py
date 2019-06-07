ff1a15973888816b25c4517cfff2901fca3e566b
marreA <alemarrerodiaz@gmail.com>
2019-06-07 11:17:05 +0100

    Creating the lab

---
+++ oplink/algorithms/team/scripts/laboratory/parser.py
@@ -0,0 +1,23 @@
+
+import json
+
+# Constantes de un experimento
+JOBS = "jobs"
+ALG = "Algorithm"
+PROB = "Problem"
+EVAL = "Evaluations"
+STEPS = "Steps"
+OUTPUT_DIR = "OutputDir"
+OUTPUT_FILE = "OutputFilePattern"
+REPS = "Repetitions"
+PROB_ARGS = "ProblemArgs"
+ALG_ARGS = "AlgorithmArgs"
+MUTATION = "Mutation"
+CROSSOVER = "Crossover"
+LOCAL_SEARCH = "LocalSearch"
+DECOMPOSITION = "Decomposition"
+
+
+def parse_instance(instance):
+    experiment_list = []
+    return experiment_list
