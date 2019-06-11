6e63728a869189a3bbe958b90cb208a21ade7ce3
marreA <alemarrerodiaz@gmail.com>
2019-06-11 14:58:26 +0200

    Running only in one single machine. As it usually does

--- oplink/algorithms/team/scripts/laboratory/intern.py
+++ oplink/algorithms/team/scripts/laboratory/intern.py
@@ -15,23 +15,18 @@ warnings.filterwarnings(action='ignore', module='.*paramiko.*')
 
 class Intern:
     """
-        Clase que representa a un becario. El que se encarga de ejecutar todo
-        lo que le mandan.
+        Clase que representa a un becario.
+        El que se encarga de ejecutar todo lo que le mandan.
     """
 
-    def __init__(self, experiment_list=None, machines=["localhost"], max_workers=20):
+    def __init__(self, experiment_list=None, machine="localhost", max_workers=10):
         self.experiment_list = experiment_list
-        self.machines = machines
+        self.machine = machine
         self.max_workers = max_workers
 
     def run_experiments(self):
-        if len(self.machines) == 1:
-            # Ejecutar en la misma maquina pero paralelo si es posible
-            self.__run_experiments(self.experiment_list, self.machines[0])
-
-        else:
-            exps_per_machine = len(self.experiment_list) // len(self.machines)
-            print(f"Exps per machine: {exps_per_machine}")
+        self.__run_experiments(self.experiment_list,
+                               self.machine, self.max_workers)
 
     def __add_experiment_to_queue(self, experiment, machine, exp_queue):
         th = threading.Thread(
@@ -39,34 +34,34 @@ class Intern:
         th.start()
         exp_queue.put(th)
 
-    def __run_experiments(self, experiment_list, machine, batch_size=10):
+    def __run_experiments(self, experiment_list, machine, workers=10):
         """
-            Ejecuta la cola de experimentos en la maquina que le toca
+            Ejecuta los experimentos de la lista en la maquina que se pasa
+            como parametro en grupos de batch_size inicialmente.
+            Cuando un experimento acaba, otro lo sustituye automaticamente.
         """
         exp_queue = Queue()
 
-        for i in experiment_list[:batch_size]:
+        for i in experiment_list[:workers]:
             self.__add_experiment_to_queue(i, machine, exp_queue)
-            # th = threading.Thread(
-            #     target=self.__run_ssh_experiment, args=(i, machine))
-            # th.start()
-            # exp_queue.put(th)
 
-        counter = batch_size
-        while counter < len(self.experiment_list):  # not exp_queue.empty():
+        counter = workers
+        while counter < len(experiment_list):
             th = exp_queue.get()
             th.join()
             exp_queue.task_done()
 
             self.__add_experiment_to_queue(
-                self.experiment_list[counter + 1], machine, exp_queue)
+                experiment_list[counter + 1], machine, exp_queue)
             counter += 1
-            print(f"{counter}/{len(self.experiment_list)} experiments performed")
+            print(f"{counter}/{len(experiment_list)} experiments performed")
 
         logging.info("Done")
 
     def __run_ssh_experiment(self, exp, machine):
-
+        """
+            Ejecuta el experimento sobre SSH en la maquina especificada
+        """
         logging.info(f"\n- Running:\n{exp}\nat: {machine}")
         ssh = SSHClient()
         ssh.load_system_host_keys()
