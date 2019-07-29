65d46d3a5f6b6775ed4c05cff5dbefe5a61ba27e
amarrerod <alemarrerodiaz@gmail.com>
2019-07-29 20:39:44 +0100

    Some changes in GA and KP

--- metco/src/plugins/algorithms/MonoGA/MonoGA.cpp
+++ metco/src/plugins/algorithms/MonoGA/MonoGA.cpp
@@ -8,188 +8,175 @@
  *
  **********************************************************************************/
 
+#include "MonoGA.h"
 #include <math.h>
 #include <signal.h>
 #include <cstdlib>
 #include <iostream>
-
-#include "MonoGA.h"
+#include "omp.h"
 
 // Algorithm initialization
 bool MonoGA::init(const vector<string> &params) {
-    // Check number of parameters
-    if (params.size() != 4) {
-        cout << "Parametros: popSize pm pc survivalSelection" << endl;
-        cout << "Survival Selection: " << endl;
-        cout << "   0: Steady State" << endl;
-        cout << "   1: Generational" << endl;
-        cout << "   2: Replace worst from parent and offspring" << endl;
-        return false;
-    }
-    // Only mono-objective optimization is supported
-    if (getSampleInd()->getNumberOfObj() != 1) {
-        cout << "Multi-Objective not supported" << endl;
-        return false;
-    }
+  // Check number of parameters
+  if (params.size() != 4) {
+    cout << "Parametros: popSize pm pc survivalSelection" << endl;
+    cout << "Survival Selection: " << endl;
+    cout << "   0: Steady State" << endl;
+    cout << "   1: Generational" << endl;
+    cout << "   2: Replace worst from parent and offspring" << endl;
+    return false;
+  }
+  // Only mono-objective optimization is supported
+  if (getSampleInd()->getNumberOfObj() != 1) {
+    cout << "Multi-Objective not supported" << endl;
+    return false;
+  }
 
-    // Parameters initialization
-    setPopulationSize(atoi(params[0].c_str()));
-    this->pm = atof(params[1].c_str());
-    this->pc = atof(params[2].c_str());
-    this->survivalSelectionType = atof(params[3].c_str());
+  // Parameters initialization
+  setPopulationSize(atoi(params[0].c_str()));
+  this->pm = atof(params[1].c_str());
+  this->pc = atof(params[2].c_str());
+  this->survivalSelectionType = atof(params[3].c_str());
 
-    // Chunk of code to generate random individuals
-    /*Individual *ind = getSampleInd()->internalClone();
-for (int i = 0; i < getPopulationSize(); i++) {
-            ind->restart();
-
-for (int j = 0; j < ind->getNumberOfVar(); j++)
-  cout << round(ind->getVar(j)) << " ";
-cout << endl;
-}
-return false;*/
-
-    return true;
+  return true;
 }
 
 void MonoGA::evaluateOffspring() {
-    for (int i = 0; i < offSpring.size(); i++) {
-        evaluate(offSpring[i]);
-    }
+  for (int i = 0; i < offSpring.size(); i++) {
+    evaluate(offSpring[i]);
+  }
 }
 
 // MonoGA generation
 void MonoGA::runGeneration() {
-    sortPopulation();  // Se ordena cada vez porque el paralelo podria
-                       // desordenarlo TODO: ordenar solo 1 vez
-    createOffSprings();
-    evaluateOffspring();
-    survivalSelection();
+  sortPopulation();  // Se ordena cada vez porque el paralelo podria
+                     // desordenarlo TODO: ordenar solo 1 vez
+  createOffSprings();
+  evaluateOffspring();
+  survivalSelection();
 }
 
 void MonoGA::sortPopulation() {
-    sort(population->begin(), population->end(), sortByObj0);
+  sort(population->begin(), population->end(), sortByObj0);
 }
 
 void MonoGA::binaryTournamentSelection(
     int &p1, int &p2) {  // Supone que population esta ordenada
-    int opt1 =
-        (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
-    int opt2 =
-        (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
-    p1 = min(opt1, opt2);
-    opt1 = (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
-    opt2 = (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
-    p2 = min(opt1, opt2);
+  int opt1 = (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
+  int opt2 = (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
+  p1 = min(opt1, opt2);
+  opt1 = (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
+  opt2 = (int)(((double)(getPopulationSize())) * rand() / (RAND_MAX + 1.0));
+  p2 = min(opt1, opt2);
 }
 
 void MonoGA::parentSelection(int &p1, int &p2) {
-    binaryTournamentSelection(p1, p2);
+  binaryTournamentSelection(p1, p2);
 }
 
 void MonoGA::createOffSpring() {  // Crear 2 hijos
-    int p1, p2;
-    parentSelection(p1, p2);
-    Individual *of1 = (*population)[p1]->internalClone();
-    Individual *of2 = (*population)[p2]->internalClone();
-    double vcross = rand() / (RAND_MAX + 1.0);
-    if (vcross < pc) {
-        of1->crossover(of2);
-    }
-    of1->mutation(pm);
-    of2->mutation(pm);
-    offSpring.push_back(of1);
-    offSpring.push_back(of2);
+  int p1, p2;
+  parentSelection(p1, p2);
+  Individual *of1 = (*population)[p1]->internalClone();
+  Individual *of2 = (*population)[p2]->internalClone();
+  double vcross = rand() / (RAND_MAX + 1.0);
+  if (vcross < pc) {
+    of1->crossover(of2);
+  }
+  of1->mutation(pm);
+  of2->mutation(pm);
+
+  offSpring.push_back(of1);
+  offSpring.push_back(of2);
 }
 
 void MonoGA::createOffSprings() {
-    offSpring.clear();
-    if (survivalSelectionType == SURVIVAL_SELECTION_SS) {  // Crear un unico
-                                                           // hijo
-        createOffSpring();
-        delete (offSpring[1]);
-        offSpring.pop_back();
-    } else if ((survivalSelectionType == SURVIVAL_SELECTION_GENERATIONAL) ||
-               (survivalSelectionType ==
-                SURVIVAL_SELECTION_REPLACEWORST_FROMPARENTANDOFFSPRING)) {
-        if (survivalSelectionType ==
-            SURVIVAL_SELECTION_GENERATIONAL) {  // Elitism
-            offSpring.push_back((*population)[0]->internalClone());
-        }
-        while (offSpring.size() < getPopulationSize()) {
-            createOffSpring();
-        }
+  offSpring.clear();
+  if (survivalSelectionType == SURVIVAL_SELECTION_SS) {  // Crear un unico
+                                                         // hijo
+    createOffSpring();
+    delete (offSpring[1]);
+    offSpring.pop_back();
+  } else if ((survivalSelectionType == SURVIVAL_SELECTION_GENERATIONAL) ||
+             (survivalSelectionType ==
+              SURVIVAL_SELECTION_REPLACEWORST_FROMPARENTANDOFFSPRING)) {
+    if (survivalSelectionType == SURVIVAL_SELECTION_GENERATIONAL) {  // Elitism
+      offSpring.push_back((*population)[0]->internalClone());
     }
-    if (offSpring.size() > getPopulationSize()) {
-        delete (offSpring[offSpring.size() - 1]);
-        offSpring.pop_back();
+    while (offSpring.size() < getPopulationSize()) {
+      createOffSpring();
     }
+  }
+  if (offSpring.size() > getPopulationSize()) {
+    delete (offSpring[offSpring.size() - 1]);
+    offSpring.pop_back();
+  }
 }
 
 void MonoGA::survivalSelection() {
-    if (survivalSelectionType == SURVIVAL_SELECTION_SS) {
-        if ((*population)[0]->getInternalOptDirection(0) == MINIMIZE) {
-            if ((*population)[getPopulationSize() - 1]->getObj(0) >
-                offSpring[0]->getObj(0)) {
-                delete (*population)[getPopulationSize() - 1];
-                (*population)[getPopulationSize() - 1] =
-                    offSpring[0];  // Ojo: Queda desornado
-            } else {
-                delete offSpring[0];
-            }
-        } else {  // Maximizar
-            if ((*population)[getPopulationSize() - 1]->getObj(0) <
-                offSpring[0]->getObj(0)) {
-                delete (*population)[getPopulationSize() - 1];
-                (*population)[getPopulationSize() - 1] =
-                    offSpring[0];  // Ojo: Queda desornado
-            } else {
-                delete offSpring[0];
-            }
-        }
-    } else if (survivalSelectionType == SURVIVAL_SELECTION_GENERATIONAL) {
-        for (int i = 0; i < getPopulationSize(); i++) {
-            delete (*population)[i];
-            (*population)[i] = offSpring[i];
-        }
-    } else if (survivalSelectionType ==
-               SURVIVAL_SELECTION_REPLACEWORST_FROMPARENTANDOFFSPRING) {
-        // Nota este replace worst no es el que esta explicado en el libro de
-        // Eiben Aqui nos estamos "cargando" los peores entre padres e hijos En
-        // el libro de Eiben dice que se generan nu hijos (< n) y se borran los
-        // nu peores padres. Habria que hacer otro operador mas y a este
-        // llamarlo RPELACEWORSTFROMPARENTSANDOFFSPRING Al otro lo podemos
-        // llamar GENITOR Nota: en la pagina 27 si llama replace worst a un
-        // esquema en que se eligen los mejores entre padres e hijos, pero puede
-        // llevar a confusion...
-        for (int i = 0; i < offSpring.size(); i++) {
-            population->push_back(offSpring[i]);
-        }
-        sortPopulation();
-        for (int i = 0; i < offSpring.size(); i++) {
-            delete ((*population)[population->size() - 1]);
-            population->pop_back();
-        }
+  if (survivalSelectionType == SURVIVAL_SELECTION_SS) {
+    if ((*population)[0]->getInternalOptDirection(0) == MINIMIZE) {
+      if ((*population)[getPopulationSize() - 1]->getObj(0) >
+          offSpring[0]->getObj(0)) {
+        delete (*population)[getPopulationSize() - 1];
+        (*population)[getPopulationSize() - 1] =
+            offSpring[0];  // Ojo: Queda desornado
+      } else {
+        delete offSpring[0];
+      }
+    } else {  // Maximizar
+      if ((*population)[getPopulationSize() - 1]->getObj(0) <
+          offSpring[0]->getObj(0)) {
+        delete (*population)[getPopulationSize() - 1];
+        (*population)[getPopulationSize() - 1] =
+            offSpring[0];  // Ojo: Queda desornado
+      } else {
+        delete offSpring[0];
+      }
     }
+  } else if (survivalSelectionType == SURVIVAL_SELECTION_GENERATIONAL) {
+    for (int i = 0; i < getPopulationSize(); i++) {
+      delete (*population)[i];
+      (*population)[i] = offSpring[i];
+    }
+  } else if (survivalSelectionType ==
+             SURVIVAL_SELECTION_REPLACEWORST_FROMPARENTANDOFFSPRING) {
+    // Nota este replace worst no es el que esta explicado en el libro de
+    // Eiben Aqui nos estamos "cargando" los peores entre padres e hijos En
+    // el libro de Eiben dice que se generan nu hijos (< n) y se borran los
+    // nu peores padres. Habria que hacer otro operador mas y a este
+    // llamarlo RPELACEWORSTFROMPARENTSANDOFFSPRING Al otro lo podemos
+    // llamar GENITOR Nota: en la pagina 27 si llama replace worst a un
+    // esquema en que se eligen los mejores entre padres e hijos, pero puede
+    // llevar a confusion...
+    for (int i = 0; i < offSpring.size(); i++) {
+      population->push_back(offSpring[i]);
+    }
+    sortPopulation();
+    for (int i = 0; i < offSpring.size(); i++) {
+      delete ((*population)[population->size() - 1]);
+      population->pop_back();
+    }
+  }
 }
 
 // OJO: Para optimizacion multi-modal esto habr� que cambiarlo
 void MonoGA::getSolution(MOFront *p) {
-    sortPopulation();
-    p->insert((*population)[0]);
+  sortPopulation();
+  p->insert((*population)[0]);
 }
 
 void MonoGA::printInfo(ostream &os) const {
-    os << "MonoObjective Genetic Algorithm" << endl;
-    os << "Number of Evaluations = " << getEvaluations() << endl;
-    os << "Mutation Probability = " << pm << endl;
-    os << "Crossover Probability = " << pc << endl;
-    os << "Population Size = " << getPopulationSize() << endl;
-    os << "Survival Selection Type = " << survivalSelectionType << endl;
+  os << "MonoObjective Genetic Algorithm" << endl;
+  os << "Number of Evaluations = " << getEvaluations() << endl;
+  os << "Mutation Probability = " << pm << endl;
+  os << "Crossover Probability = " << pc << endl;
+  os << "Population Size = " << getPopulationSize() << endl;
+  os << "Survival Selection Type = " << survivalSelectionType << endl;
 }
 
 bool sortByObj0(const Individual *i1, const Individual *i2) {
-    return (i1->getInternalOptDirection(0) == MINIMIZE)
-               ? (i1->getObj(0) < i2->getObj(0))
-               : (i1->getObj(0) > i2->getObj(0));
+  return (i1->getInternalOptDirection(0) == MINIMIZE)
+             ? (i1->getObj(0) < i2->getObj(0))
+             : (i1->getObj(0) > i2->getObj(0));
 }
--- metco/src/plugins/problems/Knapsack/Knapsack.cpp
+++ metco/src/plugins/problems/Knapsack/Knapsack.cpp
@@ -1,6 +1,6 @@
 /* -----------------------------------------------------------------------------
  *  One-dimensional Knapsack Problem implementation
- * 
+ *
  *  @Author: Alejandro Marrero Diaz (alu0100825008@ull.edu.es)
  *
  * -------------------------------------------------------------------------- */
@@ -19,74 +19,77 @@ const int Knapsack::ARGS = 2;
 
 // Problem Initialization
 bool Knapsack::init(const vector<string>& params) {
-    // Check for the problem parameters
-    if (params.size() != ARGS) {
-        cout << "Error Knapsack: Incorrect parameters " << endl;
-        cout << "<instance> <type_crossover>" << endl;
-        return false;
-    }
-    string filename = params[0];
-    setNumberOfObj(1);
-    type_crossover = atoi(params[1].c_str());
-    // Reads a problem from file
-    readFile(filename);
-    ratios();
-    return true;
+  // Check for the problem parameters
+  if (params.size() != ARGS) {
+    cout << "Error Knapsack: Incorrect parameters " << endl;
+    cout << "<instance> <type_crossover>" << endl;
+    return false;
+  }
+  string filename = params[0];
+  setNumberOfObj(1);
+  type_crossover = atoi(params[1].c_str());
+  // Reads a problem from file
+  readFile(filename);
+  ratios();
+  return true;
 }
 
 // Reads a problem from file
 void Knapsack::readFile(const string filename) {
-    ifstream input(filename.c_str());
-    string readLine;
-    input >> nItems;
-    input >> capacity;
-    profits.resize(nItems);
-    weights.resize(nItems);
-    deleteOrder.resize(nItems);
-    setNumberOfVar(nItems);
-    float weight, profit;
-    for (int i = 0; i < nItems; i++) {
-        deleteOrder[i] = i;
-        input >> weight >> profit;
-        weights[i] = weight;
-        profits[i] = profit;
-    }
-    input.close();
+  ifstream input(filename.c_str());
+  string readLine;
+  input >> nItems;
+  input >> capacity;
+  profits.resize(nItems);
+  weights.resize(nItems);
+  deleteOrder.resize(nItems);
+  setNumberOfVar(nItems);
+  float weight, profit;
+  for (int i = 0; i < nItems; i++) {
+    deleteOrder[i] = i;
+    input >> weight >> profit;
+    weights[i] = weight;
+    profits[i] = profit;
+  }
+  input.close();
 }
 
 // Genetic Operators - Binary Mutation
 void Knapsack::dependentMutation(double pm) {
-    mutate_binary(pm);
-    repair();
+  mutate_binary(pm);
+  repair();
 }
 
-// Genetic Operators - Uniform Crossover
+/* // Genetic Operators - Uniform Crossover
 void Knapsack::dependentCrossover(Individual* ind) {
     if (type_crossover == CROSSOVER_UNIFORM) {
         crossover_uniform(ind);
     } else if (type_crossover == CROSSOVER_HUX) {
         crossover_HUX(ind);
     } else {
-        cout << "Error en el tipo de Crossover. type_crossover = 0 -> crossover_uniforme. type_crossover = 1 -> crossover_hux" << endl;
+        cout << "Error en el tipo de Crossover. type_crossover = 0 ->
+crossover_uniforme. type_crossover = 1 -> crossover_hux" << endl;
     }
 
     ((Knapsack*)ind)->repair();
     repair();
 }
-
-void Knapsack::crossover_HUX(Individual* ind) {
+ */
+/* void Knapsack::crossover_HUX(Individual* ind) {
     int indDistance = hammingDistance(this, ind);
     vector<int> idxVector;
 
-    //Se genera un vector de valores aleatorios para seleccionar de forma aleatoria la primera
+    //Se genera un vector de valores aleatorios para seleccionar de forma
+aleatoria la primera
     //mitad de los elementos
     generateRandomValues(0, indDistance - 1, indDistance, idxVector);
 
     for (unsigned int i = 0; i < getNumberOfVar(); i++) {
         //Se cruza si los elementos son diferentes
         if (getVar(i) != ind->getVar(i)) {
-            //Se comprueba si el elemento a cruzar esta en la primera mitad de la lista aleatoria
-            for (unsigned int j = 0; j < floor(idxVector.size() / 2); j++) {
+            //Se comprueba si el elemento a cruzar esta en la primera mitad de
+la lista aleatoria for (unsigned int j = 0; j < floor(idxVector.size() / 2);
+j++) {
                 //Si esta en la lista se realiza el cruce
                 if (i == idxVector[j]) {
                     double tmp = ind->getVar(i);
@@ -98,97 +101,91 @@ void Knapsack::crossover_HUX(Individual* ind) {
     }
     ((Knapsack*)ind)->repair();
     repair();
-}
+} */
 
 // Random generation of an individual
 void Knapsack::restart(void) {
-    for (int i = 0; i < nItems; i++) {
-        setVar(i, ((double)rand() / (double)RAND_MAX));
-        setVar(i, getVar(i) * (getMaximum(i) - getMinimum(i)) + getMinimum(i));
-        if (getVar(i) >= 0.5)
-            setVar(i, 1);
-        else
-            setVar(i, 0);
-    }
-    repair();
+  for (int i = 0; i < nItems; i++) {
+    setVar(i, ((double)rand() / (double)RAND_MAX));
+    setVar(i, getVar(i) * (getMaximum(i) - getMinimum(i)) + getMinimum(i));
+    if (getVar(i) >= 0.5)
+      setVar(i, 1);
+    else
+      setVar(i, 0);
+  }
+  repair();
 }
 
 // Repairs an individual with capacity restriction
 void Knapsack::repair(void) {
-    int i = 0;
-    while (!checkCapacity()) {
-        setVar(deleteOrder[i], 0);
-        i++;
-    }
+  int i = 0;
+  while (!checkCapacity()) {
+    setVar(deleteOrder[i], 0);
+    i++;
+  }
 }
 
 // Calculates the max profit/weight ratio for each item in increasing order
 void Knapsack::ratios(void) {
-    vector<float> maxRatio(nItems, -1.0);
-    for (int j = 0; j < nItems; j++) {
-        float ratio = getProfit(j) / getWeight(j);
-        if (ratio >= maxRatio[j])
-            maxRatio[j] = ratio;
-    }
-    quicksort(deleteOrder, maxRatio, 0, nItems - 1);
-    maxRatio.shrink_to_fit();
+  vector<float> maxRatio(nItems, -1.0);
+  for (int j = 0; j < nItems; j++) {
+    float ratio = getProfit(j) / getWeight(j);
+    if (ratio >= maxRatio[j]) maxRatio[j] = ratio;
+  }
+  quicksort(deleteOrder, maxRatio, 0, nItems - 1);
+  maxRatio.shrink_to_fit();
 }
 
 // Quicksort algorithm to order the max profit/weight ratio vector
-void Knapsack::quicksort(vector<int>& deleteOrder, vector<float>& maxRatio, int begin, int end) {
-    float pivote = maxRatio[end];
-    int i = begin;
-    int j = end;
-    bool cont = true;
+void Knapsack::quicksort(vector<int>& deleteOrder, vector<float>& maxRatio,
+                         int begin, int end) {
+  float pivote = maxRatio[end];
+  int i = begin;
+  int j = end;
+  bool cont = true;
 
-    if (begin >= end)
-        return;
+  if (begin >= end) return;
 
-    while (cont) {
-        while (maxRatio[++i] < pivote)
-            ;
-        while (maxRatio[--j] > pivote)
-            ;
-        if (i < j) {
-            float temp = maxRatio[i];
-            int aux = deleteOrder[i];
-            maxRatio[i] = maxRatio[j];
-            deleteOrder[i] = deleteOrder[j];
-            maxRatio[j] = temp;
-            deleteOrder[j] = aux;
-        } else
-            cont = false;
-    }
+  while (cont) {
+    while (maxRatio[++i] < pivote)
+      ;
+    while (maxRatio[--j] > pivote)
+      ;
+    if (i < j) {
+      float temp = maxRatio[i];
+      int aux = deleteOrder[i];
+      maxRatio[i] = maxRatio[j];
+      deleteOrder[i] = deleteOrder[j];
+      maxRatio[j] = temp;
+      deleteOrder[j] = aux;
+    } else
+      cont = false;
+  }
 
-    float temp = maxRatio[i];
-    int aux = deleteOrder[i];
-    maxRatio[i] = maxRatio[end];
-    deleteOrder[i] = deleteOrder[end];
-    maxRatio[end] = temp;
-    deleteOrder[end] = aux;
+  float temp = maxRatio[i];
+  int aux = deleteOrder[i];
+  maxRatio[i] = maxRatio[end];
+  deleteOrder[i] = deleteOrder[end];
+  maxRatio[end] = temp;
+  deleteOrder[end] = aux;
 
-    quicksort(deleteOrder, maxRatio, begin, i - 1);
-    quicksort(deleteOrder, maxRatio, i + 1, end);
+  quicksort(deleteOrder, maxRatio, begin, i - 1);
+  quicksort(deleteOrder, maxRatio, i + 1, end);
 }
 
 // Checks all capacities restrictions
 bool Knapsack::checkCapacity(void) {
-    float sum = 0;
-    for (int j = 0; j < nItems; j++)
-        sum += (getWeight(j) * getVar(j));
-    if (sum > capacity)
-        return false;
-    return true;
+  float sum = 0;
+  for (int j = 0; j < nItems; j++) sum += (getWeight(j) * getVar(j));
+  if (sum > capacity) return false;
+  return true;
 }
 
 // Evaluation of an individual
 void Knapsack::evaluate(void) {
-    float objective = 0;
-    for (int j = 0; j < nItems; j++)
-        objective += profits[j] * getVar(j);
-    setObj(0, objective);
+  float objective = 0;
+  for (int j = 0; j < nItems; j++) objective += profits[j] * getVar(j);
+  setObj(0, objective);
 }
 
-Individual* Knapsack::clone(void) const {
-    return new Knapsack();
-}
+Individual* Knapsack::clone(void) const { return new Knapsack(); }
